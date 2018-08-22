5bc24cccd9d267f21f4f5bbde6f80e2b44866d1d
alaamigdady <alaamohammad283@gmail.com>
2018-08-22 15:08:00 +0300

    BCCamp

---
+++ BCCamp.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.18;
+
+contract BCCamp {
+    
+// Declare variables that will store information of BlockChain mini boot camp students, this information includes name, age, 
+// and if he/she is enjoying or not, every student information should be bound with his/her ethereum account address.
+    
+// your code here
+
+
+    
+    function addStudent(address _address, uint _age, string name, bool _isEnjoying) public {
+       // your code here
+    }
+
+
+
+    // build getStudents() function
+    
+    
+
+    // build getStudent(address _address) function
+    
+
+
+
+    // build countStudents() function
+
+    
+    
+}
